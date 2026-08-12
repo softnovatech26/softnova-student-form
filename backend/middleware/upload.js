7e2b2d062b3dcf88b043a48c1325aba7953bfa0b
Bintesiddique <areebasiddique021@gmail.com>
2026-08-12 12:14:08 +0500

    Refactor upload middleware to use memory storage instead of disk storage

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,15 +1,9 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
+const storage = multer.memoryStorage();
 
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage });
+const upload = multer({ storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
+   });
 
 export default upload;
